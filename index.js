9538cba759a9f82bb875ef392efbb256dac0dc68
valyastriz <valyastriz@gmail.com>
2024-07-14 20:02:54 -0400

    added questions prompts const

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo text: ',
+        validate: input => input.length <= 3 || 'Text must be no longer than 3 characters long'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape: ',
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter your desired shaped color: '
+    }
+];
+
